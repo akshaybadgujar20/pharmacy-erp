4d852104a3602d6a88964d2f1bd4776d4783cc25
Akshay Badgujar <abadgujar2@slb.com>
2025-12-20 18:33:11 +0530

    theme integration ready

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import {provideAnimations} from '@angular/platform-browser/animations';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import Aura from '@primeuix/themes/aura';
+import {routes} from './app.routes';
+import {providePrimeNG} from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimations()
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p'
+        }
+      }
+    })
   ]
 };
--- src/app/pages/inventory/inventory.component.ts
+++ src/app/pages/inventory/inventory.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import {Component} from '@angular/core'
+import {FormsModule} from '@angular/forms'
+import {TableModule} from 'primeng/table'
+import {InputTextModule} from 'primeng/inputtext'
+import {ButtonModule} from 'primeng/button'
+import {TagModule} from 'primeng/tag'
 
 
 @Component({
